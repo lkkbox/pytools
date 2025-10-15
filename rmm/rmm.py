6c160298d5bae38d17837d8d69386feca461bbb6
leecw <cwleetaiwan@gmail.com>
2025-10-15 08:57:31 +0800

    add calphase

--- rmm/rmm.py
+++ rmm/rmm.py
@@ -50,25 +50,38 @@ def cal_pc(
     return pc1, pc2
 
 
+def cal_phase(pc1, pc2):
+    angleDeg = np.angle(pc1 + 1j * pc2) / np.pi * 180
+    phase = np.nan * np.ones_like(pc1)
+
+    phase[( 0<= angleDeg) & (angleDeg < 45)] = 5
+    phase[( 45<= angleDeg) & (angleDeg < 90)] = 6
+    phase[( 90<= angleDeg) & (angleDeg < 135)] = 7
+    phase[( 135<= angleDeg) & (angleDeg <= 180)] = 8
+    phase[( -180<= angleDeg) & (angleDeg < -135)] = 1
+    phase[( -135<= angleDeg) & (angleDeg < -90)] = 2
+    phase[( -90<= angleDeg) & (angleDeg < -45)] = 3
+    phase[( -45<= angleDeg) & (angleDeg < 0)] = 4
+    return phase
+
+
 def _remove_previous_120(data:dict[str:np.ndarray]) -> dict[str:np.ndarray]:
-    def remove_runmean(y0:np.ndarray):
-        nt = y0.shape[0]
-        y1 = np.nan * np.ones(y0.shape)
-
-        for it in range(nt):
-            if it < 120:
-                ts = 0
-            else:
-                ts = it - 120
-
-            y1[it, :] = y0[it, :] - np.nanmean(y0[ts:it, :], axis=0)
-
-        return y1
-
     return {
-        vn: remove_runmean(data[vn])
+        vn: remove_previous_runmean(data[vn], n=120)
         for vn in var_names
     }
 
-    
-            
+
+def remove_previous_runmean(y0:np.ndarray, n:int):
+    nt = y0.shape[0]
+    y1 = np.nan * np.ones(y0.shape)
+
+    for it in range(nt):
+        if it < n:
+            ts = 0
+        else:
+            ts = it - n
+
+        y1[it, :] = y0[it, :] - np.nanmean(y0[ts:it, :], axis=0)
+
+    return y1
