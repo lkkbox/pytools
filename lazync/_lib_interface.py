8996c37b2be3f02e4aa2eb6a5a25ea47886cc9a1
lkkbox <lkkbox@mail.com>
2025-10-30 17:17:56 +0800

    add readatt

--- lazync/_lib_interface.py
+++ lazync/_lib_interface.py
@@ -6,7 +6,26 @@ def get_var_names(h:nc.Dataset) -> tuple[str]:
     return tuple(h.variables.keys())
 
 
+def get_att_names(h:nc.Dataset, varName:str) -> tuple[str]:
+    if varName == '/': # file root attributes
+        hVar = h
+    else:                     
+        hVar = h[varName]
+
+    return tuple(hVar.ncattrs())
+
+
 # ---- readers
-def read_all(h:nc.Dataset, varName:str) -> np.ndarray:
+def read_var_all(h:nc.Dataset, varName:str) -> np.ndarray:
     return np.array(h[varName][:])
 
+def read_var_slices(h:nc.Dataset, varName:str, slices:list[slice]) -> np.ndarray:
+    return np.array(h[varName][slices])
+
+
+def read_att(h:nc.Dataset, varName:str, attName:str) -> np.ndarray:
+    if varName == '/': # file root attributes
+        hVar = h
+    else:                     
+        hVar = h[varName]
+    return hVar.getncattr(attName)
--- lazync/reader.py
+++ lazync/reader.py
@@ -4,16 +4,49 @@ import numpy as np
 import os
 
 
-def read(
+def _error_if_not_file(path:str) -> None:
+    if not os.path.isfile(os.path.realpath(path)):
+        raise FileNotFoundError(f'{path=}')
+
+
+def _error_if_not_variable(h:nc.Dataset, varName:str) -> None:
+    if varName not in lib.get_var_names(h):
+        raise KeyError(f'{varName=} not found in {h.path=}')
+
+
+def _error_if_not_attribute(h:nc.Dataset, varName:str, attName:str) -> None:
+    if attName not in lib.get_att_names(h):
+        raise KeyError(f'{attName=} not found for {varName=} in {h.path=}')
+
+
+def read_var(
     path:str, 
     varName:str, 
     slices:None | list[slice]=None
 ) -> np.ndarray:
 
-    if not os.path.isfile(os.path.realpath(path)):
-        raise FileNotFoundError(f'{path=}')
+    _error_if_not_file(path)
 
     with nc.Dataset(path, 'r') as h:
-        if varName not in lib.get_var_names(h):
-            raise KeyError(f'{varName=} not found in {path=}')
-        return lib.read_all(h, varName)
+
+        _error_if_not_variable(h, varName)
+
+        if slices is None:
+            read = lib.read_var_all
+        else:
+            read = lib.read_var_slices
+
+        return read(h, varName)
+
+
+def readatt(
+    path:str, 
+    varName:str, 
+) -> str | int | float:
+
+    _error_if_not_file(path)
+
+    with nc.Dataset(path, 'r') as h:
+        _error_if_not_variable(h, varName)
+        _error_if_not_attribute(h, varName, attName)
+        return lib.read_att(h, varName, attName)
